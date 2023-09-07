901f76dc5e50b61188ed2abd648af820bb6dabda
cazz <cazzwastaken@gmail.com>
2023-09-07 23:10:34 +0200

    Fixed team joing issue.

--- cheat/core/functions/createmove.cpp
+++ cheat/core/functions/createmove.cpp
@@ -1,10 +1,12 @@
 #include "../hooks.h"
+#include "../../../external/x86retspoof/x86RetSpoof.h"
 
 bool __stdcall h::CreateMove(float frameTime, UserCmd* cmd)
 {
-	const auto result = CreateMoveOriginal(i::clientMode, frameTime, cmd);
+	static const auto sequence = reinterpret_cast<std::uintptr_t>(m::Scan("client.dll", "FF 23"));
+	const auto result = x86RetSpoof::invokeStdcall<bool>((uintptr_t)h::CreateMoveOriginal, sequence, frameTime, cmd);
 
-	if (!cmd->commandNumber)
+	if (!cmd || !cmd->commandNumber)
 		return result;
 
 	if (result)
